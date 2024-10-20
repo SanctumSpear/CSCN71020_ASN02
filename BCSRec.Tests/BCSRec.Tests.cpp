05e6bbe84246378fc52d70b22986e5f15f112592
Kian Cloutier <kiancloutier@gmail.com>
2024-10-20 14:28:14 -0400

    added test project file

---
+++ BCSRec.Tests/BCSRec.Tests.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace BCSRecTests
+{
+	TEST_CLASS(BCSRecTests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
