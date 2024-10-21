ffba19f342d0b68c68636a13756457d068b63918
Kian Cloutier <kiancloutier@gmail.com>
2024-10-20 20:26:51 -0400

    Added task 1 tests

--- BCSRec.Tests/BCSRec.Tests.cpp
+++ BCSRec.Tests/BCSRec.Tests.cpp
@@ -1,16 +1,36 @@
 #include "pch.h"
 #include "CppUnitTest.h"
 
+extern "C" {
+#include "../BCSRec/main.h"
+}
+
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace BCSRecTests
 {
-	TEST_CLASS(BCSRecTests)
-	{
+	TEST_CLASS(getPerimeterTests) {
 	public:
-		
-		TEST_METHOD(TestMethod1)
-		{
+		TEST_METHOD(getPerimiter_ValidInputs_CorrectPerimiter) {
+			int length = 10;
+			int width = 40;
+			int expected = 60;
+
+			Assert::AreEqual( expected, getPerimeter(&length, &width) );
 		}
 	};
+
+	TEST_CLASS(getAreaTests) {
+	public:
+		TEST_METHOD(getArea_ValidInputs_CorrectArea)
+		{
+			int length = 47;
+			int width = 32;
+			int expected = 1504;
+
+			Assert::AreEqual( expected, getArea(&length, &width) );
+		}
+	};
+
+	
 }
